5e5393a2cdba239ecb4522e6c74d911312528385
Tammy0903 <tamara.calixte12@stjohns.edu>
2013-10-30 00:29:16 -0400

    First Step

---
+++ GradeBook/GradeBook/GradeBook.cs
@@ -0,0 +1,12 @@
+﻿// Fig. 4.1: GradeBook.cs
+//Class declaration with one method.
+using System;
+
+public class GradeBook
+{
+   //display a welcome message to the GradeBook user
+   public void DisplayMessage()
+   {
+      Console.WriteLine("Welcome to the Grade Book!");
+   } // end method DisplayMessage
+} // end class GradeBook
---
+++ GradeBook/GradeBook/GradeBookTest.cs
@@ -0,0 +1,14 @@
+﻿// Fig. 4.2: GradeBookTest.cs
+// Create a GradeBook object and call its DisplayMessage method.
+public class GradeBookTest
+{
+   // Main method begins program execution
+   public static void Main(string[] args)
+   {
+      // create a GradeBook object and assign it to myGradeBook
+      GradeBook myGradeBook = new GradeBook();
+
+      // call myGradeBook's DisplayMessage method
+      myGradeBook.DisplayMessage();
+   } // end Main
+} // end class GradeBookTest
